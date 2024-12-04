9d9b550b1e37ffb552be79f06a00dd531557a79d
Feng Feiyang <cjqlwy@icloud.com>
2024-12-05 02:38:59 +0800

    分数分段样式和提示词。

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'user3': 100,
     }
     var currentCharacter = '';
+    var currentUserId = '';
     var currentUsername = '';
     var currentScore = 0;
 
@@ -19,13 +20,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const usernameReturn = document.getElementById('usernameReturn');
 
     const pageManager = document.getElementById('pageManager');
+    const managerPromptUpper = document.getElementById('managerPromptUpper')
     const managerDisplay = document.getElementById('managerScore');
     const decreaseButton = document.getElementById('decrease');
     const resetButton = document.getElementById('reset');
     const managerReturn = document.getElementById('managerReturn');
     
     const pageUser = document.getElementById('pageUser');
+    const userPromptUpper = document.getElementById('userPromptUpper')
     const userDisplay = document.getElementById('userScore');
+    const userPromptLower = document.getElementById('userPromptLower')
     const userReturn = document.getElementById('userReturn');
 
     managerButton.addEventListener('click', () => {
@@ -42,14 +46,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
     usernameConfirm.addEventListener('click', () => {
         changePage();
-        currentUsername = selectUsername.options[selectUsername.selectedIndex].value;
-        currentScore = scoreDict[currentUsername];
+        currentUserId = selectUsername.options[selectUsername.selectedIndex].value;
+        currentUsername = selectUsername.options[selectUsername.selectedIndex].textContent;
+        currentScore = scoreDict[currentUserId];
+
         if (currentCharacter == 'manager') {
             managerDisplay.textContent = currentScore;
             pageManager.style.display = 'block';
+            managerPromptUpper.textContent = `${currentUsername}目前的信用分是：`;
         } else if (currentCharacter == 'user') {
             userDisplay.textContent = currentScore;
             pageUser.style.display = 'block';
+            userPromptUpper.textContent = `${currentUsername}目前的信用分是：`;
+
+            if (currentScore < 95) {
+                userDisplay.style.color = 'red';
+                userPromptLower.textContent = `${currentUsername}不能使用宿舍区卫生间！`;
+                userPromptLower.style.color = 'red';
+            } else if (currentScore < 97) {
+                userDisplay.style.color = 'orange';
+                userPromptLower.textContent = `${currentUsername}在宿舍区只能使用一楼卫生间。`;
+                userPromptLower.style.color = 'orange';
+            } else {
+                userDisplay.style.color = 'green';
+                userPromptLower.textContent = `${currentUsername}可以自由使用所有卫生间～`;
+                userPromptLower.style.color = 'green';
+            }
+
         } else {
             pageUsername.style.display = 'block';
         }        
@@ -62,13 +85,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     decreaseButton.addEventListener('click', () => {
         currentScore--;
-        scoreDict[currentUsername] = currentScore;
+        scoreDict[currentUserId] = currentScore;
         managerDisplay.textContent = currentScore;
     });
 
     resetButton.addEventListener('click', () => {
         currentScore = 100;
-        scoreDict[currentUsername] = currentScore;
+        scoreDict[currentUserId] = currentScore;
         managerDisplay.textContent = currentScore;
     });
 
