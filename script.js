2170b10fbf7ff2e3eff02eeb0284cebb4ef304d8
Feng Feiyang <cjqlwy@icloud.com>
2024-12-05 01:59:44 +0800

    完成基本逻辑。

--- script.js
+++ script.js
@@ -1,81 +1,91 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let score = 100;
+    var scoreDict = {
+        'user1': 100,
+        'user2': 100,
+        'user3': 100,
+    }
+    var currentCharacter = '';
+    var currentUsername = '';
+    var currentScore = 0;
 
+    const pageLogin = document.getElementById('pageLogin');
+    const managerButton = document.getElementById('manager');
+    const userButton = document.getElementById('user');
 
-    const scoreDisplay = document.getElementById('score');
+    const pageUsername = document.getElementById('pageUsername');
+    const selectUsername = document.getElementById('usernames');
+    const usernameConfirm = document.getElementById('usernameConfirm');
+    const usernameReturn = document.getElementById('usernameReturn');
 
-
-    // const increaseButton = document.getElementById('increase');
-
-
+    const pageManager = document.getElementById('pageManager');
+    const managerDisplay = document.getElementById('managerScore');
     const decreaseButton = document.getElementById('decrease');
-
-
     const resetButton = document.getElementById('reset');
+    const managerReturn = document.getElementById('managerReturn');
+    
+    const pageUser = document.getElementById('pageUser');
+    const userDisplay = document.getElementById('userScore');
+    const userReturn = document.getElementById('userReturn');
 
+    managerButton.addEventListener('click', () => {
+        changePage();
+        pageUsername.style.display = 'block';
+        currentCharacter = 'manager';
+    });
 
-    const customScoreInput = document.getElementById('custom-score');
+    userButton.addEventListener('click', () => {
+        changePage();
+        pageUsername.style.display = 'block';
+        currentCharacter = 'user'
+    });
 
+    usernameConfirm.addEventListener('click', () => {
+        changePage();
+        currentUsername = selectUsername.options[selectUsername.selectedIndex].value;
+        currentScore = scoreDict[currentUsername];
+        if (currentCharacter == 'manager') {
+            managerDisplay.textContent = currentScore;
+            pageManager.style.display = 'block';
+        } else if (currentCharacter == 'user') {
+            userDisplay.textContent = currentScore;
+            pageUser.style.display = 'block';
+        } else {
+            pageUsername.style.display = 'block';
+        }        
+    });
 
-    const setScoreButton = document.getElementById('set-score');
-
-
-    // increaseButton.addEventListener('click', () => {
-
-
-    //     score++;
-
-
-    //     scoreDisplay.textContent = score;
-
-
-    // });
-
+    usernameReturn.addEventListener('click', () => {
+        changePage();
+        pageLogin.style.display = 'block';
+    });
 
     decreaseButton.addEventListener('click', () => {
-
-
-        score--;
-
-
-        scoreDisplay.textContent = score;
-
-
+        currentScore--;
+        scoreDict[currentUsername] = currentScore;
+        managerDisplay.textContent = currentScore;
     });
-
 
     resetButton.addEventListener('click', () => {
-
-
-        score = 100;
-
-
-        scoreDisplay.textContent = score;
-
-
+        currentScore = 100;
+        scoreDict[currentUsername] = currentScore;
+        managerDisplay.textContent = currentScore;
     });
 
-
-    setScoreButton.addEventListener('click', () => {
-
-
-        const customScore = parseInt(customScoreInput.value);
-
-
-        if (!isNaN(customScore)) {
-
-
-            score = customScore;
-
-
-            scoreDisplay.textContent = score;
-
-
-        }
-
-
+    managerReturn.addEventListener('click', () => {
+        changePage();
+        pageUsername.style.display = 'block';
     });
 
+    userReturn.addEventListener('click', () => {
+        changePage();
+        pageUsername.style.display = 'block';
+    });
+
+    function changePage(){
+        document.querySelectorAll('.page').forEach(page => {
+            page.style.display = 'none';
+        });
+    };
 
 });
